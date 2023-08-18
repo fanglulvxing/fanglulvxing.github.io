820ef084855a9dee69c731fcfd4904f721f8431a
fanglulvxing <huiyu.chen@dukekunshan.edu.cn>
2023-08-18 09:22:35 +0800

    Update settings.py

--- filterpackage/settings.py
+++ filterpackage/settings.py
@@ -28,8 +28,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'ltfyqd9owrc!2@+2$d&805goj919uj$gtaklyr1rur_5xe@=i@'
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'ltfyqd9owrc!2@+2$d&805goj919uj$gtaklyr1rur_5xe@=i@'
+# SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
